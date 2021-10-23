4a2f72c0513961eafc074cca68744995aa93771f
Nick <lis@chaikovskie.com>
2021-10-23 04:45:33 +0300

    feat: basic theming

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,28 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { GlobalThemeOverrides, NConfigProvider } from 'naive-ui'
+
+const themeOverrides: GlobalThemeOverrides = {
+  common: {
+    primaryColor: '#3590f3',
+    primaryColorHover: '#3590f3',
+    primaryColorPressed: '#3590f3',
+    infoColor: '#28c2ff',
+    successColor: '#38b000',
+    errorColor: '#A31621',
+    warningColor: '#F4743B',
+    hoverColor: '#3590f3',
+  },
+}
+</script>
 
 <template>
-  <main class="w-full h-full">
-    <section class="flex justify-center">
-      <n-message-provider>
-        <router-view />
-      </n-message-provider>
-    </section>
-  </main>
+  <n-config-provider :theme-overrides="themeOverrides">
+    <main class="w-full h-full">
+      <section class="flex justify-center">
+        <n-message-provider>
+          <router-view />
+        </n-message-provider>
+      </section>
+    </main>
+  </n-config-provider>
 </template>
