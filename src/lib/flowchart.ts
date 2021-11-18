18ca4c8d8b814e3f685f21c1ee35835605fb1a3e
Nick <lis@chaikovskie.com>
2021-11-19 00:48:05 +0300

    feat(flowchart): schema position calculation & links calculation

---
+++ src/lib/flowchart.ts
@@ -0,0 +1,374 @@
+import { Ref } from '@vue/reactivity'
+import { AnchorSpec, OverlaySpec } from '@jsplumb/common'
+import { BrowserJsPlumbInstance, newInstance } from '@jsplumb/browser-ui'
+import { FlowchartConnector } from '@jsplumb/connector-flowchart'
+import { ECNode, Instruction } from '~/lib/schema'
+
+export type FlowchartNodeType = 'process' | 'decision'
+
+export type FlowchartNodeParent = { id: string; when?: boolean }
+
+export type FlowchartNode = {
+  id: string
+  text: string
+  position: [number, number]
+  type: FlowchartNodeType
+  parent?: FlowchartNodeParent
+  anchors?: [AnchorSpec, AnchorSpec]
+  links?: string[]
+  when?: boolean
+  childrens?: FlowchartNode[]
+}
+
+export type JSPlumbRef = Ref<BrowserJsPlumbInstance | null>
+
+export const X_OFFSET = 200 as const
+export const DECISION_DIMENSIONS = {
+  WIDTH: 280,
+  HEIGHT: 140,
+} as const
+export const PROCESS_DIMENSIONS = {
+  WIDTH: 200,
+  HEIGHT: 80,
+} as const
+
+export const jsPlumb: JSPlumbRef = ref(null)
+
+export const initJsPlumb = (container: HTMLElement | Element) => {
+  if (!jsPlumb.value) {
+    jsPlumb.value = newInstance({
+      container,
+      endpoint: 'Blank',
+      connector: {
+        type: FlowchartConnector.type,
+        options: {
+          gap: 4,
+          cornerRadius: 2,
+        },
+      },
+      paintStyle: {
+        strokeWidth: 1,
+        stroke: 'black',
+        outlineWidth: 1,
+      },
+    })
+  }
+}
+
+export const getElementById = (id: string) => {
+  return document.getElementById(id) as Element
+}
+
+const getConnectorLabelId = (id: string, type: 'yes' | 'no') => {
+  if (type === 'yes') {
+    const arrowId = `arrow_yes_${id}`
+    return {
+      id: arrowId,
+      selector: `div[jtk-overlay-id="${arrowId}"]`,
+    }
+  } else {
+    const arrowId = `arrow_no_${id}`
+    return {
+      id: arrowId,
+      selector: `div[jtk-overlay-id="${arrowId}"]`,
+    }
+  }
+}
+
+export const connectNodes = (sourceId: string, node: FlowchartNode) => {
+  const labelYes = getConnectorLabelId(sourceId, 'yes')
+  const labelNo = getConnectorLabelId(sourceId, 'no')
+  const linksCount = node.links?.length || 0
+  const isDecisionConnector = node.type === 'decision' && linksCount > 1
+
+  const overlays: OverlaySpec[] = [
+    {
+      type: 'Arrow',
+      options: {
+        location: 1,
+        width: 8,
+        length: 8,
+      },
+    },
+  ]
+
+  if (
+    node.when === true &&
+    !document.querySelector(labelYes.selector) &&
+    !isDecisionConnector
+  ) {
+    overlays.push({
+      type: 'Label',
+      options: {
+        label: 'Да',
+        location: 40,
+        id: labelYes.id,
+        cssClass: 'label',
+      },
+    })
+  }
+
+  if (
+    node.when === false &&
+    !document.querySelector(labelNo.selector) &&
+    !isDecisionConnector
+  ) {
+    overlays.push({
+      type: 'Label',
+      options: {
+        label: 'Нет',
+        location: 40,
+        id: labelNo.id,
+        cssClass: 'label',
+      },
+    })
+  }
+
+  jsPlumb.value?.connect({
+    source: getElementById(sourceId),
+    target: getElementById(node.id),
+    anchor: 'Continuous',
+    scope: sourceId,
+    endpointStyle: {
+      draggable: false,
+    },
+    anchors: node.anchors,
+    overlays,
+  })
+}
+
+export const setupConnections = (nodes: FlowchartNode[]) => {
+  const setupNode = (node: FlowchartNode) => {
+    if (node.links?.length) {
+      node.links.forEach((link) => connectNodes(link, node))
+    }
+
+    node.childrens?.forEach((children) => {
+      if (children.type === 'decision') {
+        setupNode(children)
+      } else {
+        children.links?.forEach((link) => connectNodes(link, children))
+      }
+    })
+  }
+
+  nodes.forEach((e) => setupNode(e))
+}
+
+export const setupLayout = (
+  nodes: FlowchartNode[],
+  options: {
+    layerHeight: number
+    layerBaseline: number
+    innerLayerBaselineLeft: number
+    innerLayerBaselineRight: number
+    spacingX: number
+  }
+) => {
+  let currentLayer = -1
+
+  function setupChildrens(node: FlowchartNode) {
+    const [, nodeX] = node.position
+
+    if (node.childrens?.every((children) => !children.childrens)) {
+      currentLayer += 1
+
+      const leftChilds = node.childrens?.filter((e) => e.when === true)
+      const rightChilds = node.childrens?.filter((e) => e.when === false)
+
+      leftChilds.forEach((children, index) => {
+        const nodeOffset = nodeX - 200 // width of process block
+        const spaceX = index * options.spacingX
+        const widthOffset = index * 200
+
+        const childY = currentLayer * options.layerHeight
+        const childX = nodeOffset - spaceX - widthOffset
+        children.position = [childY, childX]
+      })
+
+      rightChilds.forEach((children, index) => {
+        const nodeOffset = nodeX + 280 // width of decision
+        const spaceX = index * options.spacingX
+        const widthOffset = index * 200
+
+        const childY = currentLayer * options.layerHeight
+        const childX = nodeOffset - spaceX - widthOffset
+        children.position = [childY, childX]
+      })
+      return
+    }
+
+    node.childrens
+      ?.filter((e) => e.type === 'decision')
+      .forEach((e) => {
+        setupNode(e, true)
+      })
+
+    const lastDecision = node.childrens
+      ?.filter((e) => e.type === 'decision')
+      .reverse()[0]
+
+    let lastDecisionBorder: number
+    if (lastDecision && lastDecision.childrens) {
+      lastDecisionBorder = Math.max(
+        ...lastDecision.childrens?.map((e) => e.position[1])
+      )
+    }
+
+    node.childrens
+      ?.filter((e) => e.type === 'process')
+      .forEach((e, index) => {
+        if (lastDecisionBorder) {
+          const elementOffset = 200 + options.spacingX + 60
+          e.position = [
+            currentLayer * options.layerHeight,
+            index * 200 +
+              index * options.spacingX +
+              lastDecisionBorder +
+              elementOffset,
+          ]
+        } else {
+          e.position = [
+            currentLayer * options.layerHeight,
+            index * 200 + nodeX + index * options.spacingX,
+          ]
+        }
+      })
+  }
+
+  function setupNode(node: FlowchartNode, isInner: boolean) {
+    if (node.type === 'decision') {
+      currentLayer += 1
+
+      const getNodeBaseline = (node: FlowchartNode) => {
+        if (isInner) {
+          return node.when
+            ? options.innerLayerBaselineLeft
+            : options.innerLayerBaselineRight
+        }
+
+        return options.layerBaseline
+      }
+
+      node.position = [
+        currentLayer * options.layerHeight,
+        getNodeBaseline(node),
+      ]
+
+      if (node.childrens) {
+        setupChildrens(node)
+      }
+    }
+  }
+
+  nodes.forEach((node) => setupNode(node, false))
+}
+
+export const parseInstruction = (instruction: Instruction) => {
+  const getNodeType = (type: 'option' | 'node') => {
+    const typeMapper = {
+      option: 'process',
+      node: 'decision',
+    }
+
+    return typeMapper[type] as 'process' | 'decision'
+  }
+
+  const getNodeAnchors = (
+    when: boolean | undefined
+  ): [AnchorSpec, AnchorSpec] => {
+    if (when === true) {
+      return ['Left', 'Top']
+    } else if (when === false) {
+      return ['Right', 'Top']
+    }
+
+    // when === undefined
+    return ['Bottom', 'Top']
+  }
+
+  const parseNode = (node: ECNode, parent?: FlowchartNodeParent) => {
+    const nodeType = getNodeType(node.type)
+
+    const flowchartNode: FlowchartNode = {
+      id: node._id,
+      text: node.title,
+      type: nodeType,
+      anchors: getNodeAnchors(node.when),
+      position: [0, 0],
+      when: node.when,
+      parent,
+    }
+
+    if (node.options) {
+      flowchartNode.childrens = node.options.map((children) => {
+        return parseNode(children, {
+          id: flowchartNode.id,
+          when: flowchartNode.when,
+        })
+      })
+    }
+
+    return flowchartNode
+  }
+
+  return instruction.nodes.map((node) => {
+    return parseNode(node as ECNode)
+  })
+}
+
+export const setupInstructionLinks = (nodes: FlowchartNode[]) => {
+  const getFlattenNodes = (nodes: FlowchartNode[]) => {
+    const flattenNodes: FlowchartNode[] = []
+
+    const flattenNode = (node: FlowchartNode) => {
+      flattenNodes.push(node)
+      const order = ['process', 'decision']
+
+      node.childrens
+        ?.sort((a, b) => order.indexOf(b.type) - order.indexOf(a.type))
+        .forEach((children) => {
+          if (children.type === 'decision') {
+            flattenNode(children)
+          } else {
+            flattenNodes.push(children)
+          }
+        })
+    }
+
+    nodes.forEach((node) => flattenNode(node))
+
+    return flattenNodes
+  }
+
+  const flattenNodes = getFlattenNodes(nodes)
+
+  let lastDecision: any = null
+  let lastProcesses: any = []
+
+  flattenNodes.forEach((node) => {
+    if (node.type === 'decision') {
+      if (lastDecision === null) {
+        lastDecision = node.id
+      } else if (lastDecision !== node.id) {
+        if (lastProcesses.length) {
+          node.links = lastProcesses
+          lastProcesses = []
+          lastDecision = node.id
+          node.anchors = ['Bottom', 'Top']
+        } else {
+          node.links = [lastDecision]
+          lastDecision = node.id
+        }
+      }
+    } else if (node.type === 'process') {
+      lastProcesses.push(node.id)
+
+      // there parent id is always string
+      // as we linking process that always has parent decision
+      node.links = [node.parent?.id as string]
+    }
+  })
+
+  return nodes
+}
--- src/pages/flowchart.vue
+++ src/pages/flowchart.vue
@@ -1,398 +1,77 @@
 <script setup lang="ts">
-import { BrowserJsPlumbInstance, newInstance } from '@jsplumb/browser-ui'
-import { Ref } from '@vue/reactivity'
-import { FlowchartConnector } from '@jsplumb/connector-flowchart'
-import { AnchorSpec, OverlaySpec } from '@jsplumb/common'
+import { UploadFileInfo, useMessage } from 'naive-ui'
+import {
+  FlowchartNode,
+  initJsPlumb,
+  parseInstruction,
+  setupConnections,
+  setupInstructionLinks,
+  setupLayout,
+} from '~/lib/flowchart'
 
-const flowchart = ref(null) as Ref<Element | null>
-const instance: Ref<BrowserJsPlumbInstance | null> = ref(null)
+import { Instruction } from '~/lib/schema'
+import { FileState, uploadSchema } from '~/lib/file'
 
-type FlowchartBlock = {
-  id: string
-  text: string
-  position: [number, number]
-  anchors?: [AnchorSpec, AnchorSpec]
-  type: 'block' | 'question'
-  links?: string[]
-  when?: boolean
-}
+const nodes = ref([] as FlowchartNode[])
+const schemaName = ref('')
 
-const blocks: FlowchartBlock[] = [
-  {
-    id: 'chart_1',
-    text: 'У заявки фиксированная сумма?',
-    type: 'question',
-    position: [0, 380],
-  },
-  {
-    id: 'chart_1_1',
-    links: ['chart_1'],
-    anchors: ['Left', 'Top'],
-    when: true,
-    text: 'Фиксированная сумма для группы заявок',
-    type: 'block',
-    position: [140, 0],
-  },
-  {
-    id: 'chart_1_2',
-    links: ['chart_1'],
-    anchors: ['Left', 'Top'],
-    when: true,
-    text: 'Фиксированная сумма для одной заявки',
-    type: 'block',
-    position: [140, 220],
-  },
-  {
-    id: 'chart_1_3',
-    links: ['chart_1'],
-    anchors: ['Right', 'Top'],
-    when: false,
-    text: 'Сумма будет определяться по доп. соглашениям',
-    type: 'block',
-    position: [140, 640],
-  },
-  {
-    id: 'chart_2',
-    links: ['chart_1_3', 'chart_1_2', 'chart_1_1'],
-    anchors: ['Bottom', 'Top'],
-    text: 'Есть номер лота?',
-    type: 'question',
-    position: [280, 380],
-  },
-  {
-    id: 'chart_2_1',
-    links: ['chart_2'],
-    anchors: ['Left', 'Top'],
-    when: true,
-    text: 'Закупка в ГКПЗ утверждена?',
-    type: 'question',
-    position: [420, 180],
-  },
-  {
-    id: 'chart_2_1_1',
-    links: ['chart_2_1'],
-    anchors: ['Left', 'Top'],
-    when: true,
-    text: 'В закупке один лот',
-    type: 'block',
-    position: [560, -170],
-  },
-  {
-    id: 'chart_2_1_2',
-    links: ['chart_2_1'],
-    anchors: ['Left', 'Top'],
-    when: true,
-    text: 'В закупке несколько лотов',
-    type: 'block',
-    position: [560, 40],
-  },
-  {
-    id: 'chart_2_1_3',
-    links: ['chart_2_1'],
-    anchors: ['Right', 'Top'],
-    when: false,
-    text: 'Во внеплановой закупке один лот',
-    type: 'block',
-    position: [560, 420],
-  },
-  {
-    id: 'chart_2_2',
-    links: ['chart_2_1_3', 'chart_2_1_2', 'chart_2_1_1'],
-    anchors: ['Bottom', 'Top'],
-    text: 'Способ закупки упрощённый? (УЗ, ЕИ, РЗП)',
-    type: 'question',
-    position: [700, 180],
-  },
-  {
-    id: 'chart_2_2_1',
-    links: ['chart_2_2'],
-    anchors: ['Left', 'Top'],
-    when: true,
-    text: 'УЗ или ЕИ или РЗП',
-    type: 'block',
-    position: [840, 40],
-  },
-  {
-    id: 'chart_2_2_2',
-    links: ['chart_2_2'],
-    anchors: ['Right', 'Top'],
-    when: false,
-    text: 'Другой способ',
-    type: 'block',
-    position: [840, 420],
-  },
-  {
-    id: 'chart_2_3',
-    links: ['chart_2'],
-    anchors: ['Right', 'Top'],
-    when: false,
-    text: 'Входит в список не включаемых в ГКПЗ',
-    type: 'block',
-    position: [840, 660],
-  },
-  {
-    id: 'chart_2_4',
-    links: ['chart_2'],
-    anchors: ['Right', 'Top'],
-    when: false,
-    text: 'Биржевая закупка',
-    type: 'block',
-    position: [840, 900],
-  },
-  {
-    id: 'chart_3',
-    links: ['chart_2_3', 'chart_2_4', 'chart_2_2_1', 'chart_2_2_2'],
-    anchors: ['Bottom', 'Top'],
-    text: 'Договор с НДС?',
-    type: 'question',
-    position: [980, 380],
-  },
-  {
-    id: 'chart_3_1',
-    links: ['chart_3'],
-    anchors: ['Left', 'Top'],
-    when: true,
-    text: 'Ставка НДС будет одна?',
-    type: 'question',
-    position: [1120, 180],
-  },
-  {
-    id: 'chart_3_1_1',
-    links: ['chart_3_1'],
-    anchors: ['Left', 'Top'],
-    when: true,
-    text: 'Ставка НДС одна',
-    type: 'block',
-    position: [1260, 40],
-  },
-  {
-    id: 'chart_3_1_2',
-    links: ['chart_3_1'],
-    anchors: ['Right', 'Top'],
-    when: false,
-    text: 'Несколько ставок НДС',
-    type: 'block',
-    position: [1260, 420],
-  },
-  {
-    id: 'chart_3_3',
-    links: ['chart_3'],
-    anchors: ['Right', 'Top'],
-    when: false,
-    text: 'Сумма договора без НДС',
-    type: 'block',
-    position: [1260, 660],
-  },
-  {
-    id: 'chart_4',
-    links: ['chart_3_3', 'chart_3_1_2', 'chart_3_1_1'],
-    anchors: ['Bottom', 'Top'],
-    text: 'Сумма графика оплаты по договору равна сумме графика поставки?',
-    type: 'question',
-    position: [1400, 380],
-  },
-  {
-    id: 'chart_4_1',
-    links: ['chart_4'],
-    anchors: ['Left', 'Top'],
-    text: 'Не денежные расчеты',
-    when: true,
-    type: 'block',
-    position: [1540, 180],
-  },
-  {
-    id: 'chart_4_2',
-    links: ['chart_4'],
-    anchors: ['Right', 'Top'],
-    text: 'Денежные расчеты',
-    when: false,
-    type: 'block',
-    position: [1540, 660],
-  },
-  {
-    id: 'chart_5',
-    links: ['chart_4_2', 'chart_4_1'],
-    anchors: ['Bottom', 'Top'],
-    text: 'Необходимо контроллировать равенство суммы УСД и БГ?',
-    type: 'question',
-    position: [1680, 380],
-  },
-  {
-    id: 'chart_5_1',
-    links: ['chart_5'],
-    anchors: ['Left', 'Top'],
-    text: 'Контроль по сумме отсутствует',
-    when: true,
-    type: 'block',
-    position: [1820, 180],
-  },
-  {
-    id: 'chart_5_2',
-    links: ['chart_5'],
-    anchors: ['Right', 'Top'],
-    text: 'Контроль по сумме есть',
-    when: false,
-    type: 'block',
-    position: [1820, 660],
-  },
-  {
-    id: 'chart_6',
-    links: ['chart_5_2', 'chart_5_1'],
-    anchors: ['Bottom', 'Top'],
-    text: 'Договор будет заключен от имени ИАП в интересах двух и более БУ',
-    type: 'question',
-    position: [1960, 380],
-  },
-  {
-    id: 'chart_6_1',
-    links: ['chart_6'],
-    anchors: ['Left', 'Top'],
-    text: 'Централизованный',
-    when: true,
-    type: 'block',
-    position: [2100, 180],
-  },
-  {
-    id: 'chart_6_2',
-    links: ['chart_6'],
-    anchors: ['Right', 'Top'],
-    text: 'Не централизованный',
-    when: false,
-    type: 'block',
-    position: [2100, 660],
-  },
-]
+const fileState = ref('nofile' as FileState)
+const notification = useMessage()
 
-const getElement = (id: string): Element => {
-  return flowchart.value?.querySelector(`#${id}`) as Element
-}
+const handleFileChange = async ({
+  file,
+}: {
+  file: UploadFileInfo
+}): Promise<void> => {
+  const uploadedFile = await uploadSchema(file)
 
-const connectElement = (
-  sourceId: string,
-  targetId: string,
-  anchors: [AnchorSpec, AnchorSpec] | undefined,
-  when: boolean | undefined
-) => {
-  const overlays: OverlaySpec[] = [
-    {
-      type: 'Arrow',
-      options: {
-        location: 1,
-        width: 8,
-        length: 8,
-      },
-    },
-  ]
-
-  if (
-    when === true &&
-    !flowchart.value?.querySelector(
-      `div[jtk-overlay-id="arrow_yes_${sourceId}"]`
+  if (uploadedFile.parsedFile) {
+    nodes.value = setupInstructionLinks(
+      parseInstruction(uploadedFile.parsedFile as Instruction)
     )
-  ) {
-    overlays.push({
-      type: 'Label',
-      options: {
-        label: 'Да',
-        location: 0.1,
-        id: `arrow_yes_${sourceId}`,
-        cssClass: 'label',
-      },
+
+    setupLayout(nodes.value, {
+      layerHeight: 180,
+      layerBaseline: 500,
+      innerLayerBaselineLeft: 240,
+      innerLayerBaselineRight: 760,
+      spacingX: 10,
+    })
+
+    nextTick(() => {
+      initJsPlumb(document.querySelector('.ec-flowchart') as Element)
+
+      setupConnections(nodes.value)
     })
   }
 
-  if (
-    when === false &&
-    !flowchart.value?.querySelector(
-      `div[jtk-overlay-id="arrow_no_${sourceId}"]`
-    )
-  ) {
-    overlays.push({
-      type: 'Label',
-      options: {
-        label: 'Нет',
-        location: 0.1,
-        id: `arrow_no_${sourceId}`,
-        cssClass: 'label',
-      },
-    })
-  }
-  instance.value?.connect({
-    source: getElement(sourceId),
-    target: getElement(targetId),
-    anchor: 'Continuous',
-    scope: sourceId,
-    endpointStyle: {
-      draggable: false,
-    },
-    anchors,
-    overlays,
-  })
+  notification.success(uploadedFile.message.value)
+  fileState.value = uploadedFile.fileState.value
 }
-
-onMounted(() => {
-  nextTick(() => {
-    instance.value = newInstance({
-      container: flowchart.value || undefined,
-      endpoint: 'Blank',
-      connector: {
-        type: FlowchartConnector.type,
-        options: {
-          gap: 4,
-          cornerRadius: 2,
-        },
-      },
-      paintStyle: {
-        strokeWidth: 1,
-        stroke: 'black',
-        outlineWidth: 1,
-      },
-    })
-
-    blocks.forEach((block) => {
-      if (block.links?.length) {
-        block.links.forEach((link) => {
-          connectElement(link, block.id, block.anchors, block.when)
-        })
-      }
-    })
-  })
-})
 </script>
 
 <template>
-  <div ref="flowchart" class="ec-flowchart">
-    <template v-for="block in blocks" :key="block.id">
-      <template v-if="block.type === 'question'">
-        <ec-chart-question
-          :id="block.id"
-          class="jtk-node"
-          :top="block.position[0]"
-          :left="block.position[1] + 200"
-          :width="280"
-          :height="140"
-        >
-          {{ block.text }}
-        </ec-chart-question>
-      </template>
-      <template v-else-if="block.type === 'block'">
-        <ec-chart-block
-          :id="block.id"
-          :top="block.position[0]"
-          :left="block.position[1] + 200"
-          :width="200"
-          :height="80"
-        >
-          {{ block.text }}
-        </ec-chart-block>
-      </template>
+  <div v-if="nodes.length" class="ec-flowchart">
+    <template v-for="node in nodes" :key="node.id">
+      <ec-flowchart-node :node="node" />
     </template>
+  </div>
+  <div v-else class="w-[980px] mx-auto px-6 py-8">
+    <ec-heading class="mb-4">
+      <template v-if="schemaName">{{ schemaName }}</template>
+      <template v-else>Загрузите схему</template>
+    </ec-heading>
+    <ec-file-upload
+      v-if="fileState === 'nofile'"
+      big
+      @file-change="handleFileChange"
+    />
   </div>
 </template>
 
 <style>
 .ec-flowchart {
-  @apply flex flex-col items-center relative;
+  @apply flex flex-col items-center relative py-12;
 }
 
 .ec-flowchart .jtk-endpoint {
